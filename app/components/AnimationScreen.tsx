3394d37ee8f55a308c92875f230aa23fcaeacb66
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-04-16 07:43:22 +0200

    feat: add animated splash screen

---
+++ app/components/AnimationScreen.tsx
@@ -0,0 +1,37 @@
+import LottieView from "lottie-react-native";
+import { StyleSheet } from "react-native";
+import Animated, { FadeOut } from "react-native-reanimated";
+
+interface AnimatedScreenProps {
+  setShowAnimation: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function AnimationScreen({
+  setShowAnimation,
+}: AnimatedScreenProps) {
+  return (
+    <Animated.View
+      key="somekeyyyy"
+      exiting={FadeOut.duration(1000)}
+      style={styles.animationContainer}
+    >
+      <LottieView
+        autoPlay
+        loop={false}
+        style={{ width: "50%", height: "50%" }}
+        source={require("../assets/lottie/house-animation.json")}
+        onAnimationFinish={() => setShowAnimation(false)}
+        onAnimationFailure={(e) => console.error("Error occurred animating", e)}
+      />
+    </Animated.View>
+  );
+}
+
+const styles = StyleSheet.create({
+  animationContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 1000,
+  },
+});
