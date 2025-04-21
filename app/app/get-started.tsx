b6f224357f371b054bc8edacbec4da1dd39d29bc
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-04-21 12:35:49 +0200

    feat: replace get-started content w/ intro screens & steps

--- app/app/get-started.tsx
+++ app/app/get-started.tsx
@@ -1,41 +1,55 @@
-import ImagesCarousel from "@/components/ImagesCarousel";
 import { router } from "expo-router";
+import { useColorScheme } from "nativewind";
 import * as React from "react";
-import { Pressable, View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import Svg, { Ellipse } from "react-native-svg";
-import { useThemeContextValues } from "@/components/themes";
+import { Image } from "react-native";
+import Onboarding from "react-native-onboarding-swiper";
 
 export default function GetStarted() {
-  const { theme } = useThemeContextValues();
-
+  const { colorScheme } = useColorScheme();
   return (
-    <SafeAreaView className="flex-1 bg-secondary">
-      <View className="absolute z-10 right-0">
-        <Svg width={252} height={241}>
-          <Ellipse
-            cx={181}
-            cy={33}
-            opacity={0.6}
-            fill={theme === "light" ? "#7dd1fc" : "#0c496e"}
-            rx={181}
-            ry={178}
-          />
-        </Svg>
-      </View>
-
-      <View className="flex-1  justify-center">
-        <Text className="text-center text-primary mb-16 text-4xl z-20">
-          Welcome to RealEstate
-        </Text>
-        <ImagesCarousel />
-        <Pressable
-          className="bg-outstand w-[80%] self-center mt-16 h-16 rounded-xl"
-          onPress={() => router.push("/login")}
-        >
-          <Text className="text-center my-auto text-primary">Get Started</Text>
-        </Pressable>
-      </View>
-    </SafeAreaView>
+    <Onboarding
+      pages={[
+        {
+          backgroundColor: colorScheme === "dark" ? "#000" : "#fff",
+          image: <Image source={require("../assets/intro/step1.png")} />,
+          title: "سئمت من الوسطاء و أسعارهم؟",
+          subtitle: "مكانك الجديد في انتظارك. لا وسطاء، لا هراء.",
+          titleStyles: {
+            fontFamily: "LemonBrush",
+          },
+        },
+        {
+          backgroundColor: colorScheme === "dark" ? "#000" : "#fff",
+          image: <Image source={require("../assets/intro/step2.png")} />,
+          title: "جد مكانك، المثالي.",
+          subtitle:
+            "استأجر، اشترِ، أو بع - اكتشف أماكن تناسب حياتك. لا ضغوط، فقط خيارات ذكية.",
+          titleStyles: {
+            fontFamily: "LemonBrush",
+          },
+        },
+        {
+          backgroundColor: colorScheme === "dark" ? "#000" : "#fff",
+          image: <Image source={require("../assets/intro/step3.png")} />,
+          title: "بع بطريقة ذكية، و وفر جهود الدعاية.",
+          subtitle:
+            "احصل على أفضل سعر لمنزلك دون أي ضغوط. نحن نتولى المهمة - و أنت تحصل علي مالك.",
+          titleStyles: {
+            fontFamily: "LemonBrush",
+          },
+        },
+        {
+          backgroundColor: colorScheme === "dark" ? "#000" : "#fff",
+          image: <Image source={require("../assets/intro/step2.png")} />,
+          title: "إنسي الوسطاء و حافظ علي ربحك بالكامل",
+          subtitle: "لا يوجد وسطاء ولا سمسرة.",
+          titleStyles: {
+            fontFamily: "LemonBrush",
+          },
+        },
+      ]}
+      onDone={() => router.push("/login")}
+      onSkip={() => router.push("/login")}
+    />
   );
 }
