96ae54969d26ce1bed7789cf7b344f62be6bbcc2
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-04-28 00:09:51 +0300

    refactor: reuse dtos as much as possible

--- app/lib/dtos.ts
+++ app/lib/dtos.ts
@@ -15,33 +15,32 @@ export const loginInputDTO = z.object({
     .min(8, "Password must be at least 8 characters"),
 });
 
-export type LoginInputDTO = z.infer<typeof loginInputDTO>;
+const baseInputDTO = loginInputDTO.extend({
+  password: z.string({
+    required_error: "Password is required",
+    invalid_type_error: "Password must be a string",
+  }),
+  confirmPassword: z.string({
+    required_error: "Confirm password is required",
+    invalid_type_error: "Confirm password must be a string",
+  }),
+  firstName: z.string({
+    required_error: "First name is required",
+    invalid_type_error: "First name must be a string",
+  }),
+  lastName: z.string({
+    required_error: "Last name is required",
+    invalid_type_error: "Last name must be a string",
+  }),
+});
 
-export const registerInputDTO = loginInputDTO
-  .extend({
-    password: z.string({
-      required_error: "Password is required",
-      invalid_type_error: "Password must be a string",
-    }),
-    confirmPassword: z.string({
-      required_error: "Confirm password is required",
-      invalid_type_error: "Confirm password must be a string",
-    }),
-    firstName: z.string({
-      required_error: "First name is required",
-      invalid_type_error: "First name must be a string",
-    }),
-    lastName: z.string({
-      required_error: "Last name is required",
-      invalid_type_error: "Last name must be a string",
-    }),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
+export const registerInputDTO = baseInputDTO.refine(
+  (data) => data.password === data.confirmPassword,
+  {
     message: "Passwords do not match",
     path: ["confirmPassword"],
-  });
-
-export type RegisterInputDTO = z.infer<typeof registerInputDTO>;
+  },
+);
 
 export const updateProfileInputDTO = z.object({
   email: z.string().email(),
@@ -49,46 +48,25 @@ export const updateProfileInputDTO = z.object({
   lastName: z.string(),
 });
 
-export type UpdateProfileInputDTO = z.infer<typeof updateProfileInputDTO>;
-
-export const changePasswordInputDTO = z
-  .object({
+export const changePasswordInputDTO = baseInputDTO
+  .pick({ password: true, confirmPassword: true })
+  .extend({
     currentPassword: z.string(),
-    newPassword: z
-      .string()
-      .min(8, { message: "Password must be at least 8 characters long" })
-      .max(255, { message: "Password must be less than 255 characters long" })
-      .regex(/^[a-zA-Z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+$/, {
-        message:
-          "Password must only contain letters, numbers, and special characters",
-      }),
-    confirmPassword: z.string(),
   })
-  .refine((data) => data.newPassword === data.confirmPassword, {
+  .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
     path: ["confirmPassword"],
   });
 
-export type ChangePasswordInputDTO = z.infer<typeof changePasswordInputDTO>;
-
-export const setPasswordInputDTO = z
-  .object({
-    newPassword: z
-      .string()
-      .min(8, { message: "Password must be at least 8 characters long" })
-      .max(255, { message: "Password must be less than 255 characters long" })
-      .regex(/^[a-zA-Z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+$/, {
-        message:
-          "Password must only contain letters, numbers, and special characters",
-      }),
-    confirmPassword: z.string(),
+export const setPasswordInputDTO = baseInputDTO
+  .pick({
+    password: true,
+    confirmPassword: true,
   })
-  .refine((data) => data.newPassword === data.confirmPassword, {
+  .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
     path: ["confirmPassword"],
   });
-
-export type SetPasswordInputDTO = z.infer<typeof setPasswordInputDTO>;
 
 export const verifyInputDTO = z.object({
   code: z
@@ -96,8 +74,6 @@ export const verifyInputDTO = z.object({
     .regex(/^[a-zA-Z0-9]{3}-[a-zA-Z0-9]{3}$/)
     .transform((code) => code.toUpperCase()),
 });
-
-export type VerifyInputDTO = z.infer<typeof verifyInputDTO>;
 
 export const linkAccountDTO = z.discriminatedUnion("provider", [
   z.object({
@@ -110,4 +86,10 @@ export const linkAccountDTO = z.discriminatedUnion("provider", [
   }),
 ]);
 
+export type LoginInputDTO = z.infer<typeof loginInputDTO>;
+export type RegisterInputDTO = z.infer<typeof registerInputDTO>;
+export type UpdateProfileInputDTO = z.infer<typeof updateProfileInputDTO>;
+export type ChangePasswordInputDTO = z.infer<typeof changePasswordInputDTO>;
+export type SetPasswordInputDTO = z.infer<typeof setPasswordInputDTO>;
+export type VerifyInputDTO = z.infer<typeof verifyInputDTO>;
 export type LinkAccountDTO = z.infer<typeof linkAccountDTO>;
