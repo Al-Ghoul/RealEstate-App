93e359871c2f6d18e56b8138d23bf6b22edce12d
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-05-07 15:01:34 +0300

    ci: add types check jobs

--- nix/tests/jobs.nix
+++ nix/tests/jobs.nix
@@ -4,6 +4,28 @@
 }: let
   real-estate-app = inputs.cells.repo.packages.app;
 in {
+  types-check-job = real-estate-app.overrideAttrs {
+    name = "Types Check Job";
+    phases = [
+      "unpackPhase"
+      "configurePhase"
+      "checkPhase"
+      "installPhase"
+    ];
+    doCheck = true;
+    checkPhase = ''
+      runHook preCheck
+      bun ./node_modules/typesafe-i18n/cli/typesafe-i18n.mjs --no-watch
+      bunx tsc --noEmit --module esnext
+      runHook postCheck
+    '';
+    installPhase = ''
+      mkdir $out
+    '';
+    meta = {
+      description = "A job that runs types check on the source";
+    };
+  };
   lint-job = real-estate-app.overrideAttrs {
     name = "Lint Job";
     phases = [
