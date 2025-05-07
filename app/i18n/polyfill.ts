6b46505f8c8b6e2f20f4a947f97e5e849b7e9459
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-05-07 15:00:59 +0300

    feat: add Intl API's polyfills

---
+++ app/i18n/polyfill.ts
@@ -0,0 +1,7 @@
+import getCanonicalLocales from "@formatjs/intl-getcanonicallocales/polyfill";
+import Locale from "@formatjs/intl-locale/polyfill";
+import PluralRules from "@formatjs/intl-pluralrules/polyfill";
+import "@formatjs/intl-pluralrules/locale-data/en";
+import "@formatjs/intl-pluralrules/locale-data/ar";
+
+export { getCanonicalLocales, Locale, PluralRules };
